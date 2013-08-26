1663506cd588f8af5eb74f6592ab6d3b13415494
jakenotjacob <jake.campbell91@gmail.com>
2013-08-25 19:20:30 -0600

    Removed !necessary variables, added spacing

--- add_signature.rb
+++ add_signature.rb
@@ -2,6 +2,7 @@
 #ruby add_signature.rb (file to add sig here)
 #i.e.
 #ruby add_signature.rb file.txt
+
 sig =
 '
 ___  ||Yer pal
@@ -10,7 +11,7 @@ ___  ||Yer pal
   /v v)
 o(    )
 '
-to_sign = ARGV[0]
+
 `echo "#{ sig }" | cat - "#{ ARGV[0] }" > /tmp/out && mv /tmp/out "#{ ARGV[0] }"`
 
 
