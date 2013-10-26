22e9eb5132f3be34d486809d16d020380c183ade
jakenotjacob <jake.campbell91@gmail.com>
2013-10-26 13:59:57 -0600

    Nested Hashes example

---
+++ hashin_hashes.rb
@@ -0,0 +1,16 @@
+#Some HASHception up in this bitch
+#
+#To use as a guide for understand params calls in Rails
+
+
+x = { y: { a: 'A', b: 'B', c: 'C'} }
+
+#Get X of Y hash
+x[:y]
+
+#Get values inside of X of Y hash
+x[:y][:a]
+x[:y][:b]
+x[:y][:c]
+
+
